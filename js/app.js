81d73de99eb184565a5ab0156779bec190d6e94a
louisberns <louisuntitled@gmail.com>
2018-01-04 15:27:56 +0000

    Added initial data structure

---
+++ js/app.js
@@ -0,0 +1,38 @@
+/*
+ * Data to interate with MODEL VIEW VIEWMODEL (MVVM)
+ */
+
+// MODEL
+info = {
+	personal: {
+		name: 'Luis Guilherme Berns Silva',
+		address: 'Maringa-PR - Brazil',
+		contacts: {
+			phone: '+55 44 9 9972-5926',
+			e-mail: 'louisuntitled@gmail.com',
+			linkedin: ['https://www.linkedin.com/in/louisberns/', '/in/louisberns'],
+			github: ['https://github.com/louisberns', '/louisberns'],
+			twitter: ['https://twitter.com/louisuntitled', '@louisuntitled']
+		}
+	},
+	pro: {
+		skills: {
+			project: [
+				{name: 'SCRUM', level: 10}
+			],
+			web: [
+				{name: 'Javascript', level: 7}
+			],
+			ux: [
+				{name: 'User Research', level: 8}
+			]
+		}
+	}
+
+}
+
+// VIEWMODEL
+
+ 
+// VIEW
+
